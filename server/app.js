06d04fdcfbce24d0910f714f3acafba0ed6d8a9c
Patrick Haimbaugh <Phaimbaugh2000@gmail.com>
2017-07-10 13:55:46 -0400

    Sequelized server with postgres

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(require('./middleware/headers'));
 
@@ -10,3 +11,49 @@ app.use('/api/test', function(req, res){
 app.listen(3000, function(){
 	console.log("app is listening on 3000");
 });
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('workoutlog', 'postgres', 'hguabmiah7821', {
+	host: 'localhost',
+	dialect: 'postgres'
+});
+
+sequelize.authenticate().then(
+	function() {
+		console.log('connected to the workoutlog postgres db');
+	},
+	function(err){
+		console.log(err);
+	}
+);
+
+//build a user model in sqllize
+	var User = sequelize.define('user', {
+		username: Sequelize.STRING,
+		passwordhash: Sequelize.STRING,
+	});
+
+User.sync();
+//User.sync([ force: true ]);
+
+app.use(bodyParser.json());
+
+app.post('/api/user', function(req, res) {
+	var username = req.body.user.username;
+	var pass = req.body.user.password;
+	User.create({
+		username: username,
+		passwordhash: ""
+	}).then(
+		function createSuccess(user){
+			res.json({
+				user: user,
+				message: 'create'
+			})
+		},
+		function createError(err){
+			res.send(500, err.message);
+		}
+	);
+});
+
